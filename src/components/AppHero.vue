ade48958dd9b0492b0bc151f73abc0758a0b14fd
ValerioCarbone <valeriocarbone10@gmail.com>
2023-11-03 16:30:30 +0100

    ADD - Import component

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    props: ['testo'],
+    data() {
+        return {
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <p>{{ testo }}</p>
+        <img src="../../public/desert.jpg">
+    </div>
+</template>
+
+<style scoped></style>
